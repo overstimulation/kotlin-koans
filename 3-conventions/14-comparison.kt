10eb8bb4a568bb60e1a40221dc035b6e2360402a
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-06-29 19:18:53 +0200

    14-comparison

---
+++ 3-conventions/14-comparison.kt
@@ -0,0 +1,11 @@
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
+
+fun test(date1: MyDate, date2: MyDate) {
+    println(date1 < date2)
+}
